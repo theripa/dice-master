5522ba2628f48caa2957c8af28ba4b7e1c2f591e
akikap12ea <thepepripa7x1@gmail.com>
2025-09-18 17:21:58 -0300

    Return to loading screen after match completion

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,18 @@ function App() {
   const { gameState, handleRollDice, resetGame } = useGameLogic();
   const [gameStarted, setGameStarted] = React.useState(false);
 
+  // Return to loading screen when game is finished
+  React.useEffect(() => {
+    if (gameState.gameStatus === 'finished') {
+      const timer = setTimeout(() => {
+        setGameStarted(false);
+        resetGame();
+      }, 3000); // Wait 3 seconds to show the victory message
+      
+      return () => clearTimeout(timer);
+    }
+  }, [gameState.gameStatus, resetGame]);
+
   const handleStartGame = () => {
     setGameStarted(true);
   };
